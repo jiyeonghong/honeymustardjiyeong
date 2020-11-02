8951386cc696eea572c2de5a0cb2d8eb0ba46924
jiyeonghong <jiyeong.hong.1@gmail.com>
2020-11-02 00:45:53 -0500

    Change

--- forms/contact.php
+++ forms/contact.php
@@ -7,7 +7,7 @@
   */
 
   // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'yun.hyeok.lee@gmail.com';
+  $receiving_email_address = 'jiyeong.hong.1@gmail.com';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
